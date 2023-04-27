0e0b3870b8936150037cd8512d2b43381eced636
aleonardocardenas <90074450+aleonardocardenas@users.noreply.github.com>
2023-04-27 18:10:19 -0500

    Add files via upload
    primero

---
+++ reto3/src/main/java/com/grupog35/reto3/controlador/ScoreController.java
@@ -0,0 +1,29 @@
+package com.grupog35.reto3.controlador;
+
+import com.grupog35.reto3.model.ScoreModel;
+import com.grupog35.reto3.service.ScoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/Score")
+public class ScoreController {
+
+    @Autowired
+    ScoreService scoreService;
+
+    @GetMapping("/all")
+    public List<ScoreModel> obtenerGamas(){
+        return scoreService.obtener();
+    }
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void crearGamas(@RequestBody ScoreModel score){
+        scoreService.crear(score);
+    }
+
+}
