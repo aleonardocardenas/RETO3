f47197a0dbc07f9711ecedd05bc6b36d321ebbbf
aleonardocardenas <90074450+aleonardocardenas@users.noreply.github.com>
2023-04-27 13:26:23 -0500

    Add files via upload
    exito

---
+++ finalreto3/src/main/java/com/grupog35/reto3/controller/CarController.java
@@ -0,0 +1,48 @@
+package com.grupog35.reto3.controller;
+
+import com.grupog35.reto3.dbo.CarDbo;
+import com.grupog35.reto3.model.CarModel;
+import com.grupog35.reto3.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/Car")
+public class CarController {
+
+    @Autowired
+    CarService carService;
+
+    @GetMapping("/all")
+    public List<CarModel> obtener(){
+        return carService.obtener();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<CarModel> obtenerPorId(@PathVariable int id){
+        return carService.obtenerPorId(id);
+    }
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void crear(@RequestBody CarModel car){
+        carService.crear(car);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void eliminar(@PathVariable int id){
+        carService.eliminar(id);
+    }
+
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void actualizar(@RequestBody CarDbo carDbo){
+        carService.actualizar(carDbo);
+    }
+
+}
---
+++ finalreto3/src/main/java/com/grupog35/reto3/service/GamaService.java
@@ -0,0 +1,44 @@
+package com.grupog35.reto3.service;
+
+import com.grupog35.reto3.dbo.GamaDbo;
+import com.grupog35.reto3.model.GamaModel;
+import com.grupog35.reto3.repository.GamaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GamaService {
+
+    @Autowired
+    GamaRepository gamaRepository;
+
+    public List<GamaModel> obtener(){
+        return gamaRepository.findAll();
+    }
+
+    public void crear(GamaModel gama){
+        if(!gamaRepository.existsById(gama.getIdGama())){
+            gamaRepository.save(gama);
+        }
+    }
+
+    public void eliminar(int id){
+        gamaRepository.deleteById(id);
+    }
+
+    public void actualizar(GamaDbo gamaDbo) {
+        if(gamaRepository.existsById(gamaDbo.getIdGama())){
+            GamaModel gama = gamaRepository.findById(gamaDbo.getIdGama()).get();
+            gama.setDescription(gamaDbo.getDescription());
+            gama.setName(gamaDbo.getName());
+            gamaRepository.save(gama);
+        }
+    }
+
+    public Optional<GamaModel> obtenerPorId(int id) {
+        return gamaRepository.findById(id);
+    }
+}
